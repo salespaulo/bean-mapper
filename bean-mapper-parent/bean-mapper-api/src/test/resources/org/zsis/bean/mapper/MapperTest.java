04b2058fb11ea8864d98c55538681fb8ca5bb057
Paulo R. A. Sales <salespaulo08@gmail.com>
2015-03-18 21:28:26 -0300

    Rename project folder to API

---
+++ bean-mapper-parent/bean-mapper-api/src/test/resources/org/zsis/bean/mapper/MapperTest.java
@@ -0,0 +1,14 @@
+package org.zsis.bean.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MapperTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
